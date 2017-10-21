154b7f94e33e3a8ecf2828edb493b7a60889b07a
Bogdans Afonins <bafonins@wlan-193055.nbw.tue.nl>
2017-10-21 14:13:35 +0200

    formatting

--- src/assignment2/Polynomial.java
+++ src/assignment2/Polynomial.java
@@ -129,8 +129,8 @@ public class Polynomial {
 
         Polynomial quotient = Polynomial.init(0, this.m, 0);
         Polynomial remainder = new Polynomial(this);
-        while (remainder.getDegree() >= b.getDegree() && !remainder.isZeroPolynomial()) {
 
+        while (remainder.getDegree() >= b.getDegree() && !remainder.isZeroPolynomial()) {
             ModularInt lcR = new ModularInt(remainder.terms[remainder.getDegree()]);
             ModularInt lcB = new ModularInt(b.terms[b.getDegree()]);
             int degreeDiff = remainder.getDegree() - b.getDegree();
